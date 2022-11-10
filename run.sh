b314aac6d67ad5b9a0f1534f623bf918899a18f9
AlkhateebAdnan <45887021+AdnanAlkhateb@users.noreply.github.com>
2022-11-10 05:39:04 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+# shellcheck disable=SC2046
+# shellcheck disable=SC2006
+docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry_rpi -w /opt/baby_cry_rpi -it mikkl/rpi2-babycry:squashed bash script/run_crying_detection.sh
